6695eff107a22f47b8bb3e65300266003f0d806a
VanSHOE <rahulgargofficial@gmail.com>
2021-09-10 12:28:52 +0530

    Math of shoot added

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import random
 import reply
 import gif
+import time
 import help
 
 client = discord.Client()
@@ -12,25 +13,70 @@ client = discord.Client()
 in_pen = False
 score = 0
 user = -1
+l1 = -1
+l2 = -1
+x = -1
+
 
 async def shoot(channel):
-    react, user = await client.wait_for('reaction_add')
-    while str(user.id) == "884140236257497088":
-        # print("once")
-        react, user = await client.wait_for('reaction_add')
-    await channel.send(user.mention + "reacted with " + react.emoji)
-    if str(user.id) != user:
+    react, luser = await client.wait_for('reaction_add')
+    while str(luser.id) == "884140236257497088":
+        react, luser = await client.wait_for('reaction_add')
+    # await channel.send(user.mention + "reacted with " + react.emoji)
+    print(str(luser.id) + " Here " + str(user))
+    if luser.id != user:
         return
+    global l1, l2, x, score
+    print("Here")
+    to_defend = random.uniform(0,100)
+    if to_defend <= l1:
+        to_defend = 0
+    elif to_defend <= l2:
+        to_defend = 1
+    else:
+        to_defend = 2
 
+    user_shot = -1
+    print(react.emoji)
+
+    if react.emoji == "\N{Leftwards Black Arrow}":
+        user_shot = 0
+    elif react.emoji == "\N{Upwards Black Arrow}":
+        user_shot = 1
+    elif react.emoji == "\N{Black Rightwards Arrow}":
+        user_shot = 2
+
+    await channel.send(str(user_shot) + " " + str(to_defend))
+    if user_shot == to_defend:
+        await channel.send("You suck")
+    else:
+        await channel.send("You do not suck yet")
+        score += 1
+    if user_shot == 0:
+        l1 += x
+    elif user_shot == 1:
+        l1 -= (x / 2)
+        l2 += (x / 2)
+    elif user_shot == 2:
+        l2 -= x
+
+    await channel.send("l1 and l2: " + str(l1) + " " + str(l2))
 
 
 async def prepare(channel, user):
-    await channel.send('React to this message with the side you want to shoot towards:')
-    send = await channel.send(file=discord.File('./penalty.jpg'))
-    await send.add_reaction("\N{Leftwards Black Arrow}")
-    await send.add_reaction("\N{Upwards Black Arrow}")
-    await send.add_reaction("\N{Black Rightwards Arrow}")
-    await shoot(channel)
+    global l1, l2, x
+    l1 = 33.33
+    l2 = 66.66
+    x = random.uniform(-3, 3)
+    for i in range(10):
+        await channel.send('React to this message with the side you want to shoot towards:')
+        send = await channel.send(file=discord.File('./penalty.jpg'))
+        await send.add_reaction("\N{Leftwards Black Arrow}")
+        await send.add_reaction("\N{Upwards Black Arrow}")
+        await send.add_reaction("\N{Black Rightwards Arrow}")
+        await shoot(channel)
+        await channel.send("Your score: " + str(score))
+
 
 async def pen(channel, author):
     global in_pen
@@ -44,7 +90,11 @@ async def pen(channel, author):
     user = author.id
     await prepare(channel, user)
 
+random.seed(time.time())
+
 print("Bot up!")
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -57,6 +107,7 @@ async def on_message(message):
         await message.channel.send(help.r_help())
     elif message.content.startswith('$penalty'):
         await pen(message.channel, message.author)
+
 
 # @client.event
 # async def on_reaction_add(reaction, user):
