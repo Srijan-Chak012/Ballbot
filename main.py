1a3250dc533657fbd251b689b063e5b8989acc9e
Srijan-Chak012 <srijan.chakraborty@research.iiit.ac.in>
2021-09-10 13:00:25 +0530

    defender almost done

--- main.py
+++ main.py
@@ -15,7 +15,54 @@ score = 0
 user = -1
 l1 = -1
 l2 = -1
+d1 = -1
+d2 = -1
 x = -1
+
+
+async def defend(channel):
+    react, duser = await client.wait_for('reaction_add')
+    while str(duser.id) == "884140236257497088":
+        react, duser = await client.wait_for('reaction_add')
+    # await channel.send(user.mention + "reacted with " + react.emoji)
+    print(str(duser.id) + " Here " + str(user))
+    if duser.id != user:
+        return
+    global d1, d2, x, bot_score
+    print("Here")
+    to_score = random.uniform(0,100)
+    if to_score <= d1:
+        to_score = 0
+    elif to_score <= d2:
+        to_score = 1
+    else:
+        to_score = 2
+
+    user_defense = -1
+    print(react.emoji)
+
+    if react.emoji == "\N{Leftwards Black Arrow}":
+        user_defense = 0
+    elif react.emoji == "\N{Upwards Black Arrow}":
+        user_defense = 1
+    elif react.emoji == "\N{Black Rightwards Arrow}":
+        user_defense = 2
+
+    await channel.send(str(user_defense) + " " + str(to_score))
+    if user_defense == to_score:
+        await channel.send("You suck")
+    else:
+        await channel.send("You do not suck yet")
+        bot_score += 1
+    if user_defense == 0:
+        d1 += x
+    elif user_defense == 1:
+        d1 -= (x / 2)
+        d2 += (x / 2)
+    elif user_defense == 2:
+        d2 -= x
+
+    await channel.send("d1 and d2: " + str(d1) + " " + str(d2))
 
 
 async def shoot(channel):
@@ -62,12 +109,13 @@ async def shoot(channel):
 
     await channel.send("l1 and l2: " + str(l1) + " " + str(l2))
 
-
 async def prepare(channel, user):
     global l1, l2, x
     l1 = 33.33
     l2 = 66.66
-    x = random.uniform(-3, 3)
+    d1 = 33.33
+    d2 = 66.66
+    x = random.uniform(-2, 5)
     for i in range(10):
         await channel.send('React to this message with the side you want to shoot towards:')
         send = await channel.send(file=discord.File('./penalty.jpg'))
@@ -76,17 +124,27 @@ async def prepare(channel, user):
         await send.add_reaction("\N{Black Rightwards Arrow}")
         await shoot(channel)
         await channel.send("Your score: " + str(score))
+        await channel.send("Bot score: " + str(bot_score))
+        await channel.send('React to this message with the side you want your goalkeeper to defend towards:')
+        send = await channel.send(file=discord.File('./keeper.jpg'))
+        await send.add_reaction("\N{Leftwards Black Arrow}")
+        await send.add_reaction("\N{Upwards Black Arrow}")
+        await send.add_reaction("\N{Black Rightwards Arrow}")
+        await defend(channel)
+        await channel.send("Your score: " + str(score))
+        await channel.send("Bot score: " + str(bot_score))
 
 
 async def pen(channel, author):
     global in_pen
-    global score
+    global score, bot_score
     global user
     if in_pen:
         await channel.send("Penalty shootout already in progress!")
         return
     in_pen = True
     score = 0
+    bot_score = 0
     user = author.id
     await prepare(channel, user)
 
