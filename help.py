a9671df484d687aa34b39d92124471711e91e7c6
Srijan-Chak012 <srijan.chakraborty@research.iiit.ac.in>
2021-09-17 01:40:55 +0530

    Reply done

--- help.py
+++ help.py
@@ -7,4 +7,5 @@ Use '>' as prefix to access the commands
 **:question: help** - Displays the help menu
 **:sunglasses: gif <name>** - Displays gifs of your favourite players
 **:wink: reply <name>** - My opinions on some of the most famous footballers
+**:soccer: penalty** - Have a penalty shootout with the bot!
     '''
--- reply.py
+++ reply.py
@@ -3,10 +3,95 @@ def to_send(message):
     msg = message.split()
     con = " ".join(msg[1:])
     print(con)
-    if con == "a" or con == "a bc":
-        return 'chal gaya'
+    if con == "Ronaldo" or con == "Cristiano Ronaldo":
+        return 'Not my :goat:'
 
-    return "Not Found"
+    elif con == "Messi" or con == "Lionel Messi":
+        return 'There is only one :goat:'
+
+    elif con == "Pele":
+        return "BRB, Going to check if he's scoring goals against the dogs in my backyard"
+
+    elif con == "Maradona":
+        return 'All-time legend, rest in peace you god'
+
+    elif con == "Hazard" or con == "Eden Hazard":
+        return 'Dat ass tho :fire: :weary:'
+
+    elif con == "Thorgan Hazard":
+        return 'The better Hazard'
+
+    elif con == "Ibrahimovic" or con == "Zlatan" or con == "Zlatan Ibrahimovic":
+        return 'Pretentious prick :vomit:'
+
+    elif con == "Suarez" or con == "Luis Suarez":
+        return 'One of the greatest strikers of all time'
+    
+    elif con == "Benzema" or con == "Karim Benzema":
+        return 'One of the greatest strikers of all time'
+
+    elif con == "Aguero" or con == "Sergio Aguero":
+        return 'Aguerooooooo'
+
+    elif con == "Suarez" or con == "Luis Suarez" or con == "Lukaku" or con == "Romelu Lukaku":
+        return 'One of the greatest strikers of all time'
+    
+    elif con == "Neymar":
+        return "Cant play in the CL Quarters, busy banging my sister :thumbsup:"
+
+    elif con == "Lewandowski" or con == "Robert Lewandowski":
+        return 'One of the greatest strikers of all time'
+
+    elif con == "Kylian Mbappe" or con == "Mbappe":
+        return 'Generational talent, Project Mbappe :sunglasses:'
+
+    elif con == "Haaland" or con == "Erling Haaland":
+        return 'Unpopular Opinion: Better than Mbappe'
+
+    elif con == "Mount" or con == "Mason Mount":
+        return 'Money MASEEEE!! Better than Foden :relieved:'
+
+    elif con == "Phil Foden":
+        return 'Stockport Iniesta but Mount >> '
+
+    elif con == "Salah" or con == "Mohammed Salah":
+        return 'Best RW in the world' 
+
+    elif con == "Ramos" or con == "Sergio Ramos":
+        return 'Best Defender in the world :muscle:'
+    
+    elif con == "De Bruyne" or con == "Kevin De Bruyne":
+        return 'One of the best Midfielders in the world'
+    
+    elif con == "Kante" or con == "N'golo Kante":
+        return 'One of the best Midfielders in the world, Smile Merchant :heart:'
+    
+    elif con == "Kane" or con == "Harry Kane":
+        return 'Where trophy :joy:'
+    
+    elif con == "Van Dijk" or con == "Virgil Van Dijk":
+        return 'One of the best defenders in the world :muscle:'
+    
+    elif con == "Griezmann" or con == "Antoine Griezmann":
+        return 'LMAO Barca joke club :rofl:'
+    
+    elif con == "Iniesta" or con == "Andres Iniesta" or con == "Xavi" or con == "Luka Modric" or con == "Modric":
+        return 'Money MASEEEE!! Better than Foden :relieved:'
+
+    elif con == "Neuer" or con == "Manuel Neuer":
+        return 'Best GK in the world'
+
+    elif con == "Mendy" or con == "Courtois" or con == "Thibaut Courtois" or con == "De Gea" or con == "David De Gea":
+        return 'One of the best GKs in the world'
+
+    elif con == "Buffon" or con == "Gianluigi Buffon" or con == "Iker Casillas" or con == "Casillas":
+        return 'Historically based keepers'
+
+    elif con == "Ederson" or con == "Allison" or con == "Ter Stegen" or con == "Donnarumma" or con == "Keylor Navas":
+        return 'One of the best GKs in the world'
+
+        
+    return "What are your opinions on these footballers :eyes:"
 
 
 
